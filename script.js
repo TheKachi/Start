33baa21fe183d5ad8115bec6cfa249502c4216d5
Kachi <nzebenflorence@gmail.com>
2020-04-19 19:41:08 +0100

    adjust loop condition

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 // Declare function accepting a single number or integer 
-let newNum = [];
-
 function numCheck (num) {
-
-    if (num % 2 === 0) {
-        newNum.push("yu");
-    } else if (num % 3 === 0) {
-        newNum.push("gi");
-    } else if (num % 5 === 0) {
-        newNum.push('oh');
-    } else if (num % 2===0 && num % 3 ===0) {
-        newNum.push("yu-gi")
-    } else if (num % 2 === 0 && num % 5 === 0) {
-        newNum.push("yu-oh")
-    } else if (num % 3 === 0 && num % 5 ===0) {
-        newNum.push("gi-oh")
-    } else {
-        newNum.push(num);
-    }     
-  
+    // create new array
+    let newNum = [];
+    // loop through integer
+    for (let i = 1; i <= num; i++) {
+         if (i % 2===0 && i % 3 ===0) {
+            // replace integer index
+            newNum.push("yu-gi");
+        } else if (i % 2 === 0 && i % 5 === 0) {
+            newNum.push("yu-oh")   
+        } else if (i % 3 === 0 && i % 5 ===0) {
+            newNum.push("gi-oh")
+        } else if (i % 2 === 0) {
+            newNum.push("yu");
+        } else if (i % 3 === 0) {
+            newNum.push("gi");
+        } else if (i % 5 === 0) {
+            newNum.push('oh');
+        } else {
+            newNum.push(i);
+        }    
+    }   
     return newNum;
 }
-numCheck(100);
+
+numCheck(10);
+
+numCheck(23);
